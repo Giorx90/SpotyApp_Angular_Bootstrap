e16b0288b691985b40e6c23ae8c47de7ba5c0ffb
Jorge M <j.maldo.1990@gmail.com>
2023-07-31 18:14:35 +0200

    map

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,19 +12,32 @@ export class SpotifyService {
     console.log('Listo!!');
    }
 
-  getNewReleases() {
+  getQuery ( query: string ) {
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAKL6J1zc3Q9ETDxcN7JKD2jlYkCrGf2RWZW15YXzSKZlwFtOeASZFEo5YNPR1LB1M9iA_Uit9cknuov4EpD0sjSn7Wa_-68oPfHqGjXNjpbh5U4PY'
-    })
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });   
+      'Authorization': 'Bearer BQCrnKPeJNwoe1KnQUBnO0RZbL1dyMylNPrVk9BCg__BLRZhozuOpeGtaH8DwExN4G9igiQXXHm8dV0woqfT3aGJg2iqCSjKgoCuAOdkNZPaMIRIRlE'
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases')
+      .pipe( map( (data: any) => {
+        return data.albums.items;
+      }));  
   } 
 
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAKL6J1zc3Q9ETDxcN7JKD2jlYkCrGf2RWZW15YXzSKZlwFtOeASZFEo5YNPR1LB1M9iA_Uit9cknuov4EpD0sjSn7Wa_-68oPfHqGjXNjpbh5U4PY'
-    })
-    return this.http.get(`https://api.spotify.com/v1/search?query=${termino}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9%2Cen%3Bq%3D0.8%2Cfr%3Bq%3D0.7&offset=0&limit=10`, { headers });
+    
+    return this.getQuery(`search?query=${termino}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9%2Cen%3Bq%3D0.8%2Cfr%3Bq%3D0.7&offset=0&limit=10`)
+      .pipe( map( (data:any) => {
+        return data.artists.items;
+      }));
   }
 
 }
